3e6c70d493fc17b8a6043a0b8d3d619ee78b033f
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2023-09-20 14:40:14 +0100

    Update move.js

---
+++ move.js
@@ -0,0 +1,14 @@
+#10
+  This problem was asked by Intel.
+
+You are given an array and a target value, move all instances of that target value to the end end of the array
+
+Example
+function moveElement(arr, val){
+ 
+};
+
+// testcase
+const arr = [2, 1, 2, 2, 2, 3, 4, 2];
+console.log(moveElement(arr, 2)); // [4, 1, 3, 2, 2, 2, 2, 2]
+Explanation: All the occurences of 2 are moved at the end of the array.
