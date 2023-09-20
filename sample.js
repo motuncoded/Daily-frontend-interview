888531417d807c6564de362a8fed1b01e3965309
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2023-09-20 14:09:30 +0100

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,3 +1,12 @@
+
+/*This problem was asked by Tencent.
+
+Implement Array.prototype.sample() result gets a random element from array.
+
+Examples
+[1, 9, 8, 7].sample()
+[1, 2, 3, 4].sample()*/
+
 const sample = function () {
 
         
