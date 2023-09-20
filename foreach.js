15a0af9e7f0c8205a2ebede2af9c73d6c184dc13
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2023-09-20 14:35:42 +0100

    Update foreach.js

---
+++ foreach.js
@@ -0,0 +1,14 @@
+This problem was asked by Facebook.
+
+Please implement your own Array.prototype.forEach()
+
+Example
+const words = ["apple", "ball", "cat"];
+
+const upperCaseList = [];
+
+words.newForEach((word, index) => {
+  upperCaseList[index] = word.toUpperCase();
+});
+
+console.log(upperCaseList); // [ 'APPLE', 'BALL', 'CAT' ]
