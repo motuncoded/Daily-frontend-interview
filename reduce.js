92c645725f1cc8f33e427ff2e665870b99692239
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2023-09-21 14:11:30 +0100

    Update reduce.js

--- reduce.js
+++ reduce.js
@@ -1,3 +1,18 @@
+/*You are tasked with implementing a JavaScript function called customReduce(arr, callback, initialValue) that mimics the behavior of the built-in reduce function. The customReduce function should iterate through the elements of an array arr and apply a callback function to each element, accumulating a result based on the specified initialValue. The accumulated result should be returned at the end.
+
+Write the customReduce function and provide an example of how it can be used:
+
+function customReduce(arr, callback, initialValue) {
+  // Your code here
+}
+
+// Example usage:
+const numbers = [1, 2, 3, 4, 5];
+
+const sum = customReduce(numbers, (acc, current) => acc + current, 0);
+
+console.log(sum); // Should print 15
+*/
 const arr1 = [1, 2, 3];
 
 //const reduce = myArray.reduce((prev, curr)=> prev + curr,  initialVal)
