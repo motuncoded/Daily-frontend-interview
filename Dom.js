162727a354c0bf32ce1542f33915cae5c3d3690c
motuncoded <motunrayoadeneye@example.com>
2024-01-15 05:33:55 +0100

    refactor: learnig about DOM manipulatio

--- Dom.js
+++ Dom.js
@@ -1,23 +1,44 @@
-//DOM - Document Oblect Model  
- 
-                       
-const title = document.getElementById("main-heading");
-console.log(title)
+//DOM - Document Oblect Model
 
-const listItem = document.getElementsByClassName("title")
-console.log(listItem)
-const list = document.getElementsByTagName("li")
-console.log(list)
-const container = document.querySelector("div") // this select the first div of the the html page
-console.log(container)
+const title = document.getElementById("main-heading");
+console.log(title);
+
+const listItem = document.getElementsByClassName("title");
+console.log(listItem);
+const list = document.querySelector("li");
+console.log(list);
+const container = document.querySelector("div"); // this select the first div of the the html page
+console.log(container);
 //if selecting all divs, you should use queryselectorAll
 
 //to change the text content of the main-heading
-title.textContent = "Favorite Movie List" // Favorite Movie List
+title.textContent = "Favorite Movie List"; // Favorite Movie List
+// to use css styles
+title.style.color = "red";
+
+//to loop throught element
+for (let i = 0; i < list.length; i++) {
+  // will increses fontsize of the list element
+  list[i].style.fontSize = "3rem";
+}
+
+//create element
+
+const ul = document.querySelector("ul");
+const li = document.createElement("li");
+
+// to add elements
+ul.appendChild(li);
+
+//to modify text in html
+
+console.log(list.innerText); //MotunThe matrix
+console.log(list.textContent); //MotunThe matrix
+console.log(list.innerHTML); //<span>Motun</span>The matrix
 
 //to add a new element after the paragraph
 
-document.addEventListener("DOMContentLoaded", function () {
+/*document.addEventListener("DOMContentLoaded", function () {
     
 
 const newElement = document.createElement("li");
@@ -42,6 +63,4 @@ newElement.textContent = "Black Men";
 
 
 
-})
-
-
+})*/
